2eef679f63ec46d2b612bf619f3bc6fc23da40a8
Ivanignashin <ivanignashin@mail.ru>
2025-03-28 00:13:17 +0300

    final

--- BinarySearchTree.h
+++ BinarySearchTree.h
@@ -8,232 +8,232 @@ template <class T>
 class BinarySearchTree
 {
 private:
-    struct Node {
-        T key_;
-        Node* left_;
-        Node* right_;
-        Node* p_;
-        Node(const T& key, Node* left = nullptr, Node* right = nullptr,
-            Node* p = nullptr) :
-            key_(key), left_(left), right_(right), p_(p) {
-        }
-    };
+	struct Node {
+		T key_;
+		Node* left_;
+		Node* right_;
+		Node* p_;
+		Node(const T& key, Node* left = nullptr, Node* right = nullptr,
+			Node* p = nullptr) :
+			key_(key), left_(left), right_(right), p_(p) {
+		}
+	};
 
-    Node* root_;
-    static Node* staticroot_;
+	Node* root_;
+	static Node* staticroot_;
 
-    void clear(Node* node) {
-        if (node) {
-            clear(node->left_);
-            clear(node->right_);
-            delete node;
-        }
-    }
+	void clear(Node* node) {
+		if (node) {
+			clear(node->left_);
+			clear(node->right_);
+			delete node;
+		}
+	}
 
 public:
-    BinarySearchTree() : root_(nullptr) { staticroot_ = root_; }
+	BinarySearchTree() : root_(nullptr) { staticroot_ = root_; }
 
-    BinarySearchTree(BinarySearchTree&& other) noexcept
-        : root_(other.root_)
-    {
-        other.root_ = nullptr;
-    }
+	BinarySearchTree(BinarySearchTree&& other) noexcept
+		: root_(other.root_)
+	{
+		other.root_ = nullptr;
+	}
 
-    BinarySearchTree& operator=(BinarySearchTree&& other) noexcept {
-        if (this != &other) {
-            clear(root_);
-            root_ = other.root_;
-            other.root_ = nullptr;
-        }
-        return *this;
-    }
+	BinarySearchTree& operator=(BinarySearchTree&& other) noexcept {
+		if (this != &other) {
+			clear(root_);
+			root_ = other.root_;
+			other.root_ = nullptr;
+		}
+		return *this;
+	}
 
-    ~BinarySearchTree() {
-        clear(root_);
-    }
+	~BinarySearchTree() {
+		clear(root_);
+	}
 
-    BinarySearchTree(const BinarySearchTree&) = delete;
-    BinarySearchTree& operator=(const BinarySearchTree&) = delete;
+	BinarySearchTree(const BinarySearchTree&) = delete;
+	BinarySearchTree& operator=(const BinarySearchTree&) = delete;
 
 
-    Node* searchItem(T key) {
-        if (root_ == nullptr)
-            return nullptr;
-        Node* current = root_;
-        while (true) {
-            if (current->key_ == key)
-                return current;
-            if (current->left_ && current->left_->key_ < key)
-                current = current->left_;
-            if (current->right_ && current->right_->key_ > key)
-                current = current->right_;
-            else
-                return nullptr;
-        }
-    }
+	Node* searchItem(T key) {
+		if (root_ == nullptr)
+			return nullptr;
+		Node* current = root_;
+		while (true) {
+			if (current->key_ == key)
+				return current;
+			if (current->left_ && current->left_->key_ < key)
+				current = current->left_;
+			if (current->right_ && current->right_->key_ > key)
+				current = current->right_;
+			else
+				return nullptr;
+		}
+	}
 
-    bool insertItem(T key) {
-        if (root_ == nullptr) {
-            root_ = new Node(key);
-            staticroot_ = root_;
-            return true;
-        }
-        
-        if (searchItem(key)) 
-            return false;
+	bool insertItem(T key) {
+		if (root_ == nullptr) {
+			root_ = new Node(key);
+			staticroot_ = root_;
+			return true;
+		}
+		
+		if (searchItem(key)) 
+			return false;
 
-        Node* current = root_;
-        while (true) {
-            if (current->left_ == nullptr && key < current->key_) {
-                Node* newNode = new Node(key);
-                current->left_ = newNode;
-                return true;
-            }
-            if (current->right_ == nullptr && key > current->key_) {
-                Node* newNode = new Node(key);
-                current->right_ = newNode;
-                return true;
-            }
-            if (current->left_ && current->key_ > key) {
-                current = current->left_;
-                continue;
-            }
-            if (current->right_ && current->key_ < key) {
-                current = current->right_;
-                continue;
-            }
-        }
-    }
+		Node* current = root_;
+		while (true) {
+			if (current->left_ == nullptr && key < current->key_) {
+				Node* newNode = new Node(key);
+				current->left_ = newNode;
+				return true;
+			}
+			if (current->right_ == nullptr && key > current->key_) {
+				Node* newNode = new Node(key);
+				current->right_ = newNode;
+				return true;
+			}
+			if (current->left_ && current->key_ > key) {
+				current = current->left_;
+				continue;
+			}
+			if (current->right_ && current->key_ < key) {
+				current = current->right_;
+				continue;
+			}
+		}
+	}
 
-    void deleteItem(T key) {
-        Node* current = searchItem(key);
-        if (current == nullptr)
-            return;
-        if (current->right_ == nullptr) {
-            current = current->left_;
-            return;
-        }
-        if (current->right_->left_ == nullptr) {
-            current->right_->left_ = current->left_;
-            current = current->right_;
-            return;
-        }
-        if (current->right_->left_ != nullptr) {
-            Node* curMin = current;
-            while (curMin->left_ != nullptr) {
-                curMin = curMin->left_;
-            }
-            current = curMin;
-            deleteItem(curMin->key_);
-            return;
-        }
-    }
+	void deleteItem(T key) {
+		Node* current = searchItem(key);
+		if (current == nullptr)
+			return;
+		if (current->right_ == nullptr) {
+			current = current->left_;
+			return;
+		}
+		if (current->right_->left_ == nullptr) {
+			current->right_->left_ = current->left_;
+			current = current->right_;
+			return;
+		}
+		if (current->right_->left_ != nullptr) {
+			Node* curMin = current;
+			while (curMin->left_ != nullptr) {
+				curMin = curMin->left_;
+			}
+			current = curMin;
+			deleteItem(curMin->key_);
+			return;
+		}
+	}
 
-    void print(Node* node = staticroot_, int space = 0, int step = 4) {
-        if (node == nullptr) {
-            return;
-        }
+	void print(Node* node = staticroot_, int space = 0, int step = 4) {
+		if (node == nullptr) {
+			return;
+		}
 
-        space += step;
+		space += step;
 
-        print(node->right_, space);
+		print(node->right_, space);
 
-        std::cout << '\n';
-        for (int i = step; i < space; i++) {
-            std::cout << ' ';
-        }
-        std::cout << "--(" << node->key_ << ')' << '\n';
+		std::cout << '\n';
+		for (int i = step; i < space; i++) {
+			std::cout << ' ';
+		}
+		std::cout << "--(" << node->key_ << ')' << '\n';
 
-        print(node->left_, space);
-    }
+		print(node->left_, space);
+	}
 
-    int getNumberOfNodes(Node* node = staticroot_) {
-        int res = 0;
-        if (!node)
-            return 0;
-        if (node)
-            res++;
-        if (node->left_)
-            res += getNumberOfNodes(node->left_);
-        if (node->right_)
-            res += getNumberOfNodes(node->right_);
-        return res;
-    }
+	int getNumberOfNodes(Node* node = staticroot_) {
+		int res = 0;
+		if (!node)
+			return 0;
+		if (node)
+			res++;
+		if (node->left_)
+			res += getNumberOfNodes(node->left_);
+		if (node->right_)
+			res += getNumberOfNodes(node->right_);
+		return res;
+	}
 
-    int getHeight(Node* node = staticroot_) {
-        if (node == nullptr) {
-            return 0;
-        }
-        int leftHeight = getHeight(node->left_);
-        int rightHeight = getHeight(node->right_);
-        return std::max(leftHeight, rightHeight) + 1;
-    }
+	int getHeight(Node* node = staticroot_) {
+		if (node == nullptr) {
+			return 0;
+		}
+		int leftHeight = getHeight(node->left_);
+		int rightHeight = getHeight(node->right_);
+		return std::max(leftHeight, rightHeight) + 1;
+	}
 
-    void inorderWalkIterative() {
-        std::vector<T> result;
-        std::stack<Node*> stack;
-        Node* current = root_;
+	void inorderWalkIterative() {
+		std::vector<T> result;
+		std::stack<Node*> stack;
+		Node* current = root_;
 
-        while (current != nullptr || !stack.empty()) {
-            while (current != nullptr) {
-                stack.push(current);
-                current = current->left_;
-            }
+		while (current != nullptr || !stack.empty()) {
+			while (current != nullptr) {
+				stack.push(current);
+				current = current->left_;
+			}
 
-            current = stack.top();
-            stack.pop();
-            result.push_back(current->key_);
+			current = stack.top();
+			stack.pop();
+			result.push_back(current->key_);
 
-            current = current->right_;
-        }
+			current = current->right_;
+		}
 
-        for (T i : result) std::cout << i << ", ";
-        std::cout << '\n';
-    }
+		for (T i : result) std::cout << i << ", ";
+		std::cout << '\n';
+	}
 
-    void inorderWalkIterative(std::vector<T> &result) {
-        std::stack<Node*> stack;
-        Node* current = root_;
+	void inorderWalkIterative(std::vector<T> &result) {
+		std::stack<Node*> stack;
+		Node* current = root_;
 
-        while (current != nullptr || !stack.empty()) {
-            while (current != nullptr) {
-                stack.push(current);
-                current = current->left_;
-            }
+		while (current != nullptr || !stack.empty()) {
+			while (current != nullptr) {
+				stack.push(current);
+				current = current->left_;
+			}
 
-            current = stack.top();
-            stack.pop();
-            result.push_back(current->key_);
+			current = stack.top();
+			stack.pop();
+			result.push_back(current->key_);
 
-            current = current->right_;
-        }
-        return;
-    }
+			current = current->right_;
+		}
+		return;
+	}
 
-    void inorderWalk(Node* current = staticroot_) {
-        if (current == nullptr) return;
+	void inorderWalk(Node* current = staticroot_) {
+		if (current == nullptr) return;
 
-        inorderWalk(current->left_);
-        std::cout << current->key_ << ", ";
-        inorderWalk(current->right_);
-    }
+		inorderWalk(current->left_);
+		std::cout << current->key_ << ", ";
+		inorderWalk(current->right_);
+	}
 
-    void walkByLevels() {
-        if (!root_) return;
+	void walkByLevels() {
+		if (!root_) return;
 
-        QueueVector<Node*> q(2);
-        q.enQueue(root_);
+		QueueVector<Node*> q(2);
+		q.enQueue(root_);
 
-        while (!q.isEmpty()) {
-            Node* current = q.deQueue();
-            std::cout << current->key_ << ", ";
+		while (!q.isEmpty()) {
+			Node* current = q.deQueue();
+			std::cout << current->key_ << ", ";
 
-            if (current->left_)
-                q.enQueue(current->left_);
-            if (current->right_)
-                q.enQueue(current->right_);
-        }
-    }
+			if (current->left_)
+				q.enQueue(current->left_);
+			if (current->right_)
+				q.enQueue(current->right_);
+		}
+	}
 };
 
 template <class T>
